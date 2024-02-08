5f7d680a979fe4c3859e456e2abd3bba4f68a329
DevVitorSilva <vitor.silva001080@gmail.com>
2024-02-08 12:19:31 -0300

    created the list of employees and a loop is performed to fill the record according to the quantity entered.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import entity.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -7,6 +11,25 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.print("How many employees will be registered? ");
         int employeeNumbers = sc.nextInt();
+        List<Employee> employees = new ArrayList<>();
+        for(int i = 0; i < employeeNumbers; i++){
+            int id;
+            String name;
+            double salary;
+            Employee employee;
+            System.out.println();
+            System.out.println("Employee #" + i + ":");
+            System.out.print("Id: ");
+            id = sc.nextInt();
+            sc.nextLine();
+            System.out.print("Name: ");
+            name = sc.nextLine();
+            System.out.print("Salary: ");
+            salary = sc.nextDouble();
+            employee = new Employee(id, name, salary);
+            employees.add(employee);
+        }
+
         sc.close();
     }
 }
